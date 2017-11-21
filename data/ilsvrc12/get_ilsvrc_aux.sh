29b1fb608eb5ab672bc72652dee71c34399df442
ZengFei <felix.zeng@rock-chips.com>
2017-11-21 16:50:21 +0800

    add ACL v17.10 support

--- data/ilsvrc12/get_ilsvrc_aux.sh
+++ data/ilsvrc12/get_ilsvrc_aux.sh
@@ -12,10 +12,14 @@ cd "$DIR"
 
 echo "Downloading..."
 
-wget -c http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
+if [ -s caffe_ilsvrc12.tar.gz ]; then
+  echo "caffe_ilsvrc12.tar.gz already exists, not getting it."
+else
+  wget -c http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz -O /tmp/caffe_ilsvrc12.tar.gz.temp && mv /tmp/caffe_ilsvrc12.tar.gz.temp caffe_ilsvrc12.tar.gz
+fi
 
 echo "Unzipping..."
 
-tar -xf caffe_ilsvrc12.tar.gz && rm -f caffe_ilsvrc12.tar.gz
+tar -xf caffe_ilsvrc12.tar.gz
 
 echo "Done."
